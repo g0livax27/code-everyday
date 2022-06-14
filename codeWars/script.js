c9c1edbf84b6d6e0f9d95e482ef0db90f8130374
Gerimae Oliva <gerimae.o@gmail.com>
2022-06-14 01:32:09 -0400

    day 001 of 100

--- codeWars/script.js
+++ codeWars/script.js
@@ -1,1 +1,24 @@
 <h1>Code Wars Challenges</h1>
+
+//////////////////////
+// Day 001 - June 13 \\
+///////////////////////
+
+// function multiply(a, b){
+//   return a * b
+// }
+
+// console.log(multiply(2, 7));
+
+//////////////////////////
+// Jaden Casing Strings //
+//////////////////////////
+function toJadenCase(jadenTweet) {
+    const toJadenCase = jadenTweet.toLowerCase().split(' ');
+    for (let i = 0; i < toJadenCase.length; i++) {
+        toJadenCase[i] = toJadenCase[i].charAt(0).toUpperCase() + toJadenCase[i].substr(1);
+    }
+    return toJadenCase.join(' ');
+};
+
+console.log(toJadenCase('how can mirrors be real if our eyes aren\'t real'));
