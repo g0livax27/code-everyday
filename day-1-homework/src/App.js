b5f363e5ecc4a789060f2037e554dceb1c2f7ec3
Gerimae Oliva <gerimae.o@gmail.com>
2022-06-15 21:50:41 -0400

    creating week 1 homework folder

---
+++ day-1-homework/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+        <Main />
+        <Footer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ day-1-homework/src/components/Projects.js
@@ -0,0 +1,9 @@
+// the component function
+const Projectss = (props) => {
+
+    //The Components Returned JSX
+    return <div className="Projects"> This is Projects</div>
+}
+
+// export the component
+export default Projects
