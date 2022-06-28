78001b98a365bf3a4abbf846e50ae1596df41c02
Gerimae Oliva <gerimae.o@gmail.com>
2022-06-27 20:48:47 -0400

    day 005 of 100 days of code

--- otherCodeChallenges/script.js
+++ otherCodeChallenges/script.js
@@ -1,1 +1,24 @@
-<h1>Other Code Challenges</h1>
+{/* <h1>Other Code Challenges</h1> */ }
+
+///////////////////////
+// Day 005 - June 27 \\
+///////////////////////
+
+////////////////
+// Palindrome \\
+////////////////
+
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+function palindrome(str) {
+    return str === str.split('').reverse().join('')
+};
+
+console.log(palindrome('kayak'));
