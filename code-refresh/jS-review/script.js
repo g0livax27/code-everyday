8ab8eb811e04b426c4edf6eadba328d85a36bc81
Gerimae Oliva <gerimae.o@gmail.com>
2023-01-03 21:47:48 -0500

    completing JavaScript review questions

---
+++ code-refresh/jS-review/script.js
@@ -0,0 +1,45 @@
+/*
+1. How do we assign a value to a variable?
+Declare it using an assignment operator: let, const or var
+
+2. How do we change the value of a variable?
+Reassign the value 
+
+3. How do we assign an existing variable to a new variable?
+
+
+4. Remind me, what are declare, assign, and define?
+Declare: "let", "const", "var"
+Assign: "="
+Define: data type (#NOBUNS)
+
+5. What is pseudocoding and why should you do it?
+Blueprinting what the code should do before actually writing the code
+
+6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+50/50
+*/
+
+/*
+For all other questions that involve writing code, you can solve them via the following instructions.
+
+1. Create a variable called firstVariable
+let firstVariable
+
+2. Assign it the value of the string "Hello World"
+let firstVariable = "Hello World"
+
+3. Change the value of this variable to some number
+let firstVariable = 7
+
+4. Store the value of firstVariablein a new variable called secondVariable
+let secondVariable = firstVariable
+
+5. Change the value of secondVariableto any string
+let secondVariable = "any string"
+
+6. What is the value of firstVariable?
+
+
+7. Create a variable called yourNameand set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourNameso that it returns a new string with them concatenated.
+*/
